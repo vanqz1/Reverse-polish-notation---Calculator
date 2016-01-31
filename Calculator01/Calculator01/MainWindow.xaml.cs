39629a01765783713f744647e9bba572276f36bd
vanq <vzaycheva@gmail.com>
2016-01-31 22:57:08 +0200

    first commit

---
+++ Calculator01/Calculator01/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+
+namespace Calculator01
+{
+    public partial class MainWindow : Window
+    {
+        private bool existResult;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_To_Expression_Click(object sender, RoutedEventArgs e)
+        {
+            var keyword = (e.Source as Button).Content.ToString();
+            Expression.Text += keyword;
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = Stopwatch.StartNew();
+            Calculator calc = new Calculator();
+            List<string> record = new List<string>();
+            Stack<string> operators = new Stack<string>();
+            double result = 0;
+            try
+            {
+                 result = calc.ReadExpression(Expression.Text.Trim() + " ", 0, record, operators).Result;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+                Expression.Text = String.Empty;
+            }
+            Expression.Text = result.ToString();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            MessageBox.Show(elapsedMs.ToString());
+         }
+
+        private void Clean_Click(object sender, RoutedEventArgs e)
+        {
+            Expression.Text = String.Empty;
+        }
+    }
+}
